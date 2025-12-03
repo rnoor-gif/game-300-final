817494ae1c063c606cab40650014c3d7bcbf6e73
rnoor-gif <rnoor@student.fitchburgstate.edu>
2025-12-02 20:05:35 -0500

    Player Controls
    Added code for script

--- rnfinalproject/Assets/Scripts/ControlPlayer.cs
+++ rnfinalproject/Assets/Scripts/ControlPlayer.cs
@@ -1,16 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+
     }
 }
