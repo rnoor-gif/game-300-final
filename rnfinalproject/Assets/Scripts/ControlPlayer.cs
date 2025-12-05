1ac79b81a9e03d0c110a7e72bdd6333a4675f619
rnoor-gif <rnoor@student.fitchburgstate.edu>
2025-12-04 20:53:42 -0500

    Updated testing ground
    Added more movement and jump function to player, expanded baseplate

--- rnfinalproject/Assets/Scripts/ControlPlayer.cs
+++ rnfinalproject/Assets/Scripts/ControlPlayer.cs
@@ -7,11 +7,14 @@ public class ControlPlayer : MonoBehaviour
     
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    public float playerGravi;
+    public float jumpPow = 10.0f;
 
     
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= playerGravi;
     }
 
     
@@ -19,6 +22,13 @@ public class ControlPlayer : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        float rightInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector3.right * speed * rightInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
+        }
 
     }
 }
